7fb982091eacfe2492e8c32303d61683acd32d1d
xVemu <kamilox26@gmail.com>
2020-05-22 20:10:07 +0200

    * add notification link in settings * fix system theme at default

--- app/src/main/java/pl/vemu/zsme/moreFragment/SettingsFragment.java
+++ app/src/main/java/pl/vemu/zsme/moreFragment/SettingsFragment.java
@@ -1,10 +1,13 @@
 package pl.vemu.zsme.moreFragment;
 
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import pl.vemu.zsme.R;
@@ -18,8 +21,20 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.settings, rootKey);
         ListPreference theme = findPreference("theme");
+        Preference button = findPreference("notification");
         theme.setOnPreferenceChangeListener((preference, newValue) -> {
             AppCompatDelegate.setDefaultNightMode(Integer.parseInt((String) newValue));
+            return true;
+        });
+        button.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent().setAction("android.settings.APP_NOTIFICATION_SETTINGS");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                intent.putExtra("android.provider.extra.APP_PACKAGE", getContext().getPackageName());
+            } else {
+                intent.putExtra("app_package", getContext().getPackageName());
+                intent.putExtra("app_uid", getContext().getApplicationInfo().uid);
+            }
+            startActivity(intent);
             return true;
         });
     }
