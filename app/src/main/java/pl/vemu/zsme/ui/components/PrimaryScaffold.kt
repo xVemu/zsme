c1495ce58c1b34de100610423db9e508f7e4a851
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2024-10-16 23:16:57 +0200

    Update adaptive navigation

--- app/src/main/java/pl/vemu/zsme/ui/components/PrimaryScaffold.kt
+++ app/src/main/java/pl/vemu/zsme/ui/components/PrimaryScaffold.kt
@@ -7,67 +7,74 @@ import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.displayCutoutPadding
-import androidx.compose.foundation.layout.exclude
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.adaptive.navigationsuite.ExperimentalMaterial3AdaptiveNavigationSuiteApi
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuite
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldLayout
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.material3.adaptive.WindowAdaptiveInfo
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.material3.adaptive.navigationsuite.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.rememberNavController
-import pl.vemu.zsme.remembers.isLandscape
+import androidx.window.core.layout.WindowHeightSizeClass
+import androidx.window.core.layout.WindowWidthSizeClass
+import com.ramcosta.composedestinations.spec.NavGraphSpec
 import pl.vemu.zsme.ui.BottomNavItem
 import pl.vemu.zsme.ui.currentScreenAsState
 import pl.vemu.zsme.ui.fullScreen
 
-@OptIn(
-    ExperimentalMaterial3AdaptiveNavigationSuiteApi::class,
-)
 @Composable
 fun PrimaryScaffold(
     navController: NavController,
     content: @Composable BoxScope.() -> Unit,
 ) {
-    Scaffold(contentWindowInsets = WindowInsets.systemBars.exclude(WindowInsets.statusBars)) { padding ->
+    Surface(
+        color = NavigationSuiteScaffoldDefaults.containerColor,
+        contentColor = NavigationSuiteScaffoldDefaults.contentColor
+    ) {
+        val layoutType = currentWindowAdaptiveInfo().calculateType()
+
         NavigationSuiteScaffoldLayout(
             navigationSuite = {
                 AnimatedVisibility(
                     visible = !fullScreen,
-                    enter = enterAnim(isLandscape),
-                    exit = exitAnim(isLandscape)
+                    enter = enterAnim(layoutType != NavigationSuiteType.NavigationBar),
+                    exit = exitAnim(layoutType != NavigationSuiteType.NavigationBar)
                 ) {
-                    Navigation(navController, isLandscape)
+                    val currentDestination by navController.currentScreenAsState()
+
+                    NavigationSuite(
+                        layoutType = layoutType,
+                        content = {
+                            items(navController, currentDestination)
+                        }
+                    )
                 }
             },
-            layoutType = if (isLandscape) NavigationSuiteType.NavigationRail else NavigationSuiteType.NavigationBar,
-        ) {
-            Box(
-                modifier = Modifier
-                    .then(
-                        if (isLandscape) Modifier
-                            .displayCutoutPadding()
-                            .padding(padding) else Modifier
+            layoutType = layoutType,
+            content = {
+                Box(
+                    Modifier.consumeWindowInsets(
+                        when (layoutType) {
+                            NavigationSuiteType.NavigationBar ->
+                                NavigationBarDefaults.windowInsets.only(WindowInsetsSides.Bottom)
+
+                            NavigationSuiteType.NavigationRail ->
+                                NavigationRailDefaults.windowInsets.only(WindowInsetsSides.Start)
+
+                            NavigationSuiteType.NavigationDrawer ->
+                                DrawerDefaults.windowInsets.only(WindowInsetsSides.Start)
+
+                            else -> WindowInsets(0, 0, 0, 0)
+                        }
                     )
-                    .consumeWindowInsets(padding),
-                content = content,
-            )
-        }
+                ) {
+                    content()
+                }
+            }
+        )
     }
 }
 
@@ -77,24 +84,20 @@ private fun enterAnim(rail: Boolean) =
 private fun exitAnim(rail: Boolean) =
     fadeOut() + if (rail) slideOutHorizontally() else slideOutVertically { it / 2 }
 
-@OptIn(ExperimentalMaterial3AdaptiveNavigationSuiteApi::class)
-@Composable
-private fun Navigation(navController: NavController, rail: Boolean) {
-    val currentDestination by navController.currentScreenAsState()
 
-    NavigationSuite(
-        modifier = if (rail) Modifier.displayCutoutPadding() else Modifier,
-        layoutType = if (rail) NavigationSuiteType.NavigationRail else NavigationSuiteType.NavigationBar,
-    ) {
-        BottomNavItem.entries.forEach { item ->
-            val selected = currentDestination == item.destination
+private fun NavigationSuiteScope.items(navController: NavController, currentDestination: NavGraphSpec) {
+    BottomNavItem.entries.forEach { item ->
+        val selected = currentDestination == item.destination
 
-            item(label = { Text(stringResource(item.label)) }, selected = selected, icon = {
+        item(
+            label = { Text(stringResource(item.label)) }, selected = selected,
+            icon = {
                 Icon(
                     imageVector = if (selected) item.iconFilled else item.icon,
                     contentDescription = stringResource(item.label),
                 )
-            }, onClick = onClick@{
+            },
+            onClick = onClick@{
                 if (selected) {
                     navController.popBackStack(
                         item.destination.startRoute.route,
@@ -110,27 +113,18 @@ private fun Navigation(navController: NavController, rail: Boolean) {
                     launchSingleTop = true
                     restoreState = true
                 }
-            })
-        }
+            },
+        )
     }
 }
 
-@Preview
-@Composable
-private fun BottomBarPreview() {
-    val navController = rememberNavController()
-    Navigation(
-        navController = navController,
-        rail = false,
-    )
-}
+private fun WindowAdaptiveInfo.calculateType(): NavigationSuiteType {
+    val height = windowSizeClass.windowHeightSizeClass
+    val width = windowSizeClass.windowWidthSizeClass
 
-@Preview
-@Composable
-private fun NavigationRailPreview() {
-    val navController = rememberNavController()
-    Navigation(
-        navController = navController,
-        rail = true,
-    )
+    return when {
+        windowPosture.isTabletop -> NavigationSuiteType.NavigationBar
+        height == WindowHeightSizeClass.COMPACT || width == WindowWidthSizeClass.MEDIUM || width == WindowWidthSizeClass.EXPANDED -> NavigationSuiteType.NavigationRail
+        else -> NavigationSuiteType.NavigationBar
+    }
 }
