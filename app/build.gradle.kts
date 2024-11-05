330568a4a5580c6285c424989f902caaf989f4de
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2024-11-05 20:33:23 +0100

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -140,17 +140,17 @@ dependencies {
     // Other
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("com.google.android.play:app-update-ktx:2.1.0")
-    implementation("androidx.core:core-ktx:1.13.1") // https://developer.android.com/jetpack/androidx/releases/core
+    implementation("androidx.core:core-ktx:1.15.0") // https://developer.android.com/jetpack/androidx/releases/core
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
     implementation("androidx.startup:startup-runtime:1.2.0")
 
     // Compose
-    val compose = "1.7.4"
-    implementation("androidx.compose.material3:material3:1.3.0") // https://developer.android.com/jetpack/androidx/releases/compose-material3
+    val compose = "1.7.5"
+    implementation("androidx.compose.material3:material3:1.3.1") // https://developer.android.com/jetpack/androidx/releases/compose-material3
     implementation("androidx.compose.material:material-icons-extended:$compose") // https://developer.android.com/jetpack/androidx/releases/compose-material
     implementation("androidx.activity:activity-compose:1.9.3") // https://developer.android.com/jetpack/androidx/releases/activity
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.6") // https://developer.android.com/jetpack/androidx/releases/lifecycle
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6") // https://developer.android.com/jetpack/androidx/releases/lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7") // https://developer.android.com/jetpack/androidx/releases/lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7") // https://developer.android.com/jetpack/androidx/releases/lifecycle
     // Adaptive
     // currentWindowAdaptiveInfo()
     implementation("androidx.compose.material3.adaptive:adaptive:1.0.0") // https://developer.android.com/jetpack/androidx/releases/compose-material3-adaptive
@@ -159,7 +159,7 @@ dependencies {
     // rememberListDetailPaneScaffoldNavigator, rememberSupportingPaneScaffoldNavigator
 //    implementation("androidx.compose.material3.adaptive:adaptive-navigation:1.0.0") // https://developer.android.com/jetpack/androidx/releases/compose-material3-adaptive
     // AdaptiveNavigationBar
-    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.3.0") // https://developer.android.com/jetpack/androidx/releases/compose-material3
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.3.1") // https://developer.android.com/jetpack/androidx/releases/compose-material3
     // Test
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose")
     debugImplementation("androidx.compose.ui:ui-test-manifest:$compose")
